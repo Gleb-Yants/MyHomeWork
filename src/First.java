513d28653fb97b96b001a03d29b9c4d50c2e2dc3
Gleb-Yants <gleb.yants@gmail.com>
2016-05-30 21:51:04 +0300

    test

--- src/First.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Gleb_Yants on 30.05.2016.
- */
-public class First {
-    public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("627");
-    }
-}
---
+++ src/Module_1.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Gleb_Yants on 30.05.2016.
+ */
+public class Module_1 {
+}
