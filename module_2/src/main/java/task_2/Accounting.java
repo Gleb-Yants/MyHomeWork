224bfd774c456981ff91bd09c01dcdf75063847d
Gleb-Yants <gleb.yants@gmail.com>
2016-06-19 18:57:58 +0300

    doit

---
+++ module_2/src/main/java/task_2/Accounting.java
@@ -0,0 +1,29 @@
+package task_2;
+
+
+/**
+ * Created by Gleb_Yants on 30.05.2016.
+ */
+public class Accounting {
+    public static int checkEmployee(Employee e) {
+        int result = 0;
+        if (e.pa != null) {
+            result = e.pa.count * e.pa.cost;
+        }
+        if (e.pe != null) {
+            result += e.pe.count * e.pe.cost;
+        }
+        if (e.st != null) {
+            result += e.st.count * e.st.cost;
+        }
+        return result;
+    }
+/* just for test
+
+    public static void main(String[] args) {
+        Employee e = new Employee(new Paper(1), new Pencil(1),new Sticker(1));
+        System.out.println(checkEmployee(e));
+    }
+
+    */
+}
